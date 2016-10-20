81287999608633bd995c996883ce4eb5068dbc36
Ray Vafa <vafatabar@gmail.com>
2016-10-20 21:36:36 +1100

    setting up test

---
+++ test/index.js
@@ -0,0 +1,7 @@
+// import { isValidNumber } from './src/index.js';
+
+describe('isValidNumber', function() {
+  it('0417127384 should be valid', function() {
+      assert.equal(true, false);
+    });
+});
